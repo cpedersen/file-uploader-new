741e6b1d0654d61d4efcc8e44236a8201274db80
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-04-01 19:45:29 -0400

    Working version

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import FilterableList from './FilterableList/FilterableList';
 import './App.css';
+import { faThList } from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      filterOption: 'All'
+    };
+  }
+
+  updateSearchTerm(term) {
+    this.setState({
+      searchTerm: term
+    })
+  }
+
+  updateFilterOption(option) {
+    this.setState({
+      filterOption: option
+    })
+  }
+
   render() {
     return (
     <div className="App">
-      <SearchBar/>
-      <FilterableList files={this.props.files}/>
+      <SearchBar
+        searchTerm={this.state.searchTerm}
+        filterOption={this.state.filterOption}
+        handleUpdate={term=>this.updateSearchTerm(term)}
+        handleFilterChange={option => this.updateFilterOption(option)}/>
+      <FilterableList files={this.props.files}
+        files={this.props.files}
+        searchTerm={this.state.searchTerm}
+        filterOption={this.state.filterOption}/>
     </div>
     );
   }
